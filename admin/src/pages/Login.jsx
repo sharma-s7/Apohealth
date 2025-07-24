222d8bad73b876312a1ca423ec469a6f8ce159ae
s-sharma7 <shubhamsharmadev9@gmail.com>
2025-07-24 14:48:50 +0530

    add button

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
   return (
     <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5E5E5E] text-sm shadow-lg'>
-        <p className='text-2xl font-semibold m-auto'><span className='text-primary'>{state}</span> Login</p>
+        <p className='text-2xl font-semibold m-auto'><span className='text-blue-500'>{state}</span> Login</p>
         <div className='w-full '>
           <p>Email</p>
           <input onChange={(e) => setEmail(e.target.value)} value={email} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="email" required />
@@ -55,11 +55,11 @@ const Login = () => {
           <p>Password</p>
           <input onChange={(e) => setPassword(e.target.value)} value={password} className='border border-[#DADADA] rounded w-full p-2 mt-1' type="password" required />
         </div>
-        <button className='bg-primary text-white w-full py-2 rounded-md text-base'>Login</button>
+        <button className='bg-blue-500 text-white w-full py-2 rounded-md text-base'>Login</button>
         {
           state === 'Admin'
-            ? <p>Doctor Login? <span onClick={() => setState('Doctor')} className='text-primary underline cursor-pointer'>Click here</span></p>
-            : <p>Admin Login? <span onClick={() => setState('Admin')} className='text-primary underline cursor-pointer'>Click here</span></p>
+            ? <p>Doctor Login? <span onClick={() => setState('Doctor')} className='text-blue-500 underline cursor-pointer'>Click here</span></p>
+            : <p>Admin Login? <span onClick={() => setState('Admin')} className='text-blue-500 underline cursor-pointer'>Click here</span></p>
         }
       </div>
     </form>
